b45fb1b9dd0d7a25e9b0f49e60409638aa9827ff
Edmond Del Mundo <edmond.delmundo@gmail.com>
2020-10-15 22:19:16 +0800

    Added snapshot test from jest

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,7 +1,15 @@
 import {shallow, mount, render} from "enzyme";
 import React from "react";
 import Card from "./Card";
+import renderer from  "react-test-renderer";
 
-it("expect to render card component", () => {
+it("expect to render card component - using enzyme shallow", () => {
+    //enzyme shallow
     expect(shallow(<Card />).length).toEqual(1);
+})
+
+it("expect to render card component - using jest snapshot", () => {
+    //jest snapshot react-test-renderer
+    const tree = renderer.create(<Card />);
+    expect(tree).toMatchSnapshot();
 })
